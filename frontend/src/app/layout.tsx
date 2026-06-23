e8248ea7fef04bb11cbe198e2b69ab1f78eb76db
Ranjeet Kumar <ranjeet@growthlenz.com>
2026-06-23 23:44:56 +0530

    remove GTM noscript from root layout body

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -51,16 +51,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* End Google Tag (gtag.js) */}
       </head>
       <body className="antialiased bg-[#07080a] text-slate-100 min-h-screen" suppressHydrationWarning>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NT3DQKR3"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
